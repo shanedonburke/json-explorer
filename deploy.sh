45c2d12db5b3d6784aa09f5e528a09e998cd02d6
shanedonburke <shanedonburke@gmail.com>
2022-09-05 12:42:08 -0400

    Deploy

--- deploy.sh
+++ deploy.sh
@@ -12,8 +12,8 @@ cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git init
-git checkout -b main
+# git init
+# git checkout -b main
 git add -A
 git commit -m 'deploy'
 
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push origin gh-pages
+git push -f https://github.com/shanedonburke/json-explorer.git main:gh-pages
 
 cd -

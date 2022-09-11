56b22bf638c68a393b328c7d685ae23a091c7aba
shanedonburke <shanedonburke@gmail.com>
2022-09-11 14:07:03 -0400

    Fix deploy script

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -16,15 +16,15 @@ fi
 # build main branch
 npm run build
 
-sleep 5s
+echo '> Build finished.'
 
 # deploy
+echo '> Deploying...'
 git add --force dist
 git commit -m 'Deploy to Github Pages'
 git push origin `git subtree split --prefix dist main`:gh-pages --force
 
 # don't push dist/ to main branch
-git checkout main
 git reset --hard HEAD~1
 
 rm -rf dist/

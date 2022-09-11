ceea44b64d27ab57a60de3a6c1754c76394a0ffb
shanedonburke <shanedonburke@gmail.com>
2022-09-11 13:32:10 -0400

    Improve deploy script

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -3,10 +3,19 @@
 # abort on errors
 set -e
 
-# build
+# build main branch
+git checkout main
 npm run build
+
+# move change to gh-pages branch
+git stash
+git checkout gh-pages
+git stash pop
 
 # deploy
 git add --force dist
 git commit -m 'deploy'
 git subtree push --prefix=dist https://github.com/shanedonburke/json-explorer.git gh-pages
+
+# return to main branch
+git checkout main

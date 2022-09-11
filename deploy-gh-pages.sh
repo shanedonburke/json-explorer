dc1a59e9ce66ada67bbb734f5042157db5a87a64
shanedonburke <shanedonburke@gmail.com>
2022-09-11 13:14:15 -0400

    Clean up deploy script

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -6,11 +6,7 @@ set -e
 # build
 npm run build
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
+# deploy
 git add -A
 git commit -m 'deploy'
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
 git subtree push --prefix=dist https://github.com/shanedonburke/json-explorer.git gh-pages

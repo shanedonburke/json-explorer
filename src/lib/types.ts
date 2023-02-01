a14e0abb507238e327a78f53411a187781a54964
shanedonburke <shanedonburke@gmail.com>
2023-02-01 18:16:43 -0500

    Deploy to Github Pages

--- src/lib/types.ts
+++
@@ -1,9 +0,0 @@
-import type monaco from "monaco-editor";
-
-export type MonacoEditor = monaco.editor.IStandaloneCodeEditor;
-
-/** A model path and its value */
-export interface PathValuePair {
-  path: Array<string>;
-  value: any;
-}
